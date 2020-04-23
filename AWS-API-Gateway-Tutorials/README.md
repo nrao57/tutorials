1f278afbcc210de32459f24c317becd3d6958efa
Nikhil Rao <raonikhil57@gmail.com>
2020-04-23 14:38:02 -0400

    Adds AWS-API-Gateway-Tutorials

---
+++ AWS-API-Gateway-Tutorials/README.md
@@ -0,0 +1,8 @@
+# AWS-API-Gateway-Tutorials
+All code used in the AWS API Gateway Video Tutorials
+
+Code from API Gateway Youtube Tutorials (Youtube: [Nikhil Rao](https://www.youtube.com/channel/UCubvyvZCaLOncyGVN-pQYCg?view_as=subscriber))
+
+Amazon AWS CloudFormation Documentation
+------------
+https://docs.aws.amazon.com/cli/latest/reference/apigateway/
---
+++ AWS-API-Gateway-Tutorials/apigateway_cli.ps1
@@ -0,0 +1,7 @@
+﻿# Powershell Code for API Gateway
+aws apigateway create-rest-api --name TestAPI --description "Test API for Youtube Video"
+aws apigateway get-resources --rest-api-id "milyu87qk5"
+aws apigateway create-resource --rest-api-id "milyu87qk5" --parent-id "8abc5nneel" --path-part "new-resource"
+aws apigateway put-method --rest-api-id "milyu87qk5" --resource-id "za4bto" --http-method "GET" --authorization-type "NONE"
+aws apigateway put-integration --rest-api-id "milyu87qk5" --resource-id "za4bto" --http-method GET --type MOCK 
+aws apigateway create-deployment --rest-api-id "milyu87qk5" --stage-name "dev" 
